9c6886642e0dea7dcfc8a6087f0106baee5a7bc4
selim <selim.ozturk@afiniti.com>
2022-09-21 12:06:37 +0300

    up

--- aside/mini-projects/radar/airforce/util/file.py
+++
@@ -1,8 +0,0 @@
-def read_line_by_line(file_path):
-    content = None
-    try:
-        with open(file_path, "r") as f:
-            content = f.read().splitlines()
-    except IOError as e:
-        print(e)
-    return content
--- aside/mini-projects/radar/setup.sh
+++
@@ -1,9 +0,0 @@
-python3 -m pip install --user --upgrade pip
-
-pip3 install poetry 
-
-source .venv/bin/activate
-
-poetry install 
-
-poetry shell 
