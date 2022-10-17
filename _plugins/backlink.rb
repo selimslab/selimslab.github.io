0cb5a9c66cd95d3d78e30c8bd0190704e0853177
selim <selim.ozturk@afiniti.com>
2022-10-17 13:48:31 +0300

    up

--- _plugins/backlink.rb
+++ _plugins/backlink.rb
@@ -1,9 +1,10 @@
 class BackLinksGenerator < Jekyll::Generator
     def generate(site)
-  
+      
+      links = {}
+      titles = {}
       # Identify backlinks and add them to each doc
       site.documents.each do |current_note|
-
         notes_linking_to_current_note = site.documents.filter do |e|
             id = "[" + current_note.id.gsub(/\//, '') + "]"
             e.content.include?(id)
@@ -11,14 +12,24 @@ class BackLinksGenerator < Jekyll::Generator
 
         current_note.data['backlinks'] = notes_linking_to_current_note
 
+        links[current_note.id] = []
+        titles[current_note.id] = current_note.data["title"]
         backlinks = current_note.content.scan(/\[\[.*\]\]/)
         backlinks.each do |backlink| 
           id = backlink.gsub(/\[\[/, '').gsub(/\]\]/, '')
           link = "/" + id
+          links[current_note.id].push(link)
           title = id.gsub(/-/, ' ')
           link = "[#{title}](#{link})"
           current_note.content = current_note.content.gsub(/#{Regexp.escape(backlink)}/, link)
         end 
+
+      end
+      File.open("./assets/data/links.json","w") do |f|
+        f.write(links.to_json)
+      end
+      File.open("./assets/data/titles.json","w") do |f|
+        f.write(titles.to_json)
       end
   
     end
