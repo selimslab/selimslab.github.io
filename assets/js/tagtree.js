40395c6a224f5be95d3add368d18fec8db48f18e
Selim <hselimozturk@gmail.com>
2023-06-27 02:32:04 +0200

    up

---
+++ assets/js/tagtree.js
@@ -0,0 +1,35 @@
+---
+layout: blank
+---
+
+
+function renderTagTree(tagdata, parent) {
+    let ul = document.createElement("ul");
+    parent.appendChild(ul);
+    for (let tag in tagdata) {  
+        let li = document.createElement("li");
+        ul.appendChild(li);
+        let a = document.createElement("a");
+        li.appendChild(a);
+        a.href = "/" + tag;
+
+        if (tagnames[tag]) {
+            title = tagnames[tag];
+        }
+        else {
+            title = tag
+        }
+
+        a.innerHTML = title;
+
+        if (tagdata[tag]) {
+            renderTagTree(tagdata[tag], li);
+        }
+    }
+}
+
+const tagnames = {{ site.data.tagnames | jsonify }};
+const tagtree = {{ site.data.tagtree | jsonify }};
+const parent = document.getElementById("tagtree");
+renderTagTree(tagdata, parent);
+
