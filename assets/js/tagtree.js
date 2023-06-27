bfc56cef24401ed42a038dbd6e5217a9817551b2
Selim <hselimozturk@gmail.com>
2023-06-27 15:24:32 +0200

    up

--- assets/js/tagtree.js
+++ assets/js/tagtree.js
@@ -8,7 +8,10 @@ function renderUl(root) {
     return ul;
 }
 
+let seen = new Set();
+
 function renderLi(tag, ul) {
+    
 
     let li = document.createElement("li");
     ul.appendChild(li);
@@ -18,33 +21,34 @@ function renderLi(tag, ul) {
     if (tagnames[tag]) {
         tag = tagnames[tag];
     }
-
-    a.innerHTML = titles[tag] ?? tag;
+    seen.add(tag);
+    
+    a.innerHTML = labels[tag] ?? tag;;
     a.href = "/" + tag;
 
     li.appendChild(a);
     return li 
 }
 
-total = 0 
+
 const iterate = (obj, root) => {
     Object.keys(obj).forEach(key => {
 
     ul = renderUl(root);
     li = renderLi(key, ul);
-    total += 1;
 
     if (typeof obj[key] === 'object' && obj[key] !== null) {
             iterate(obj[key], li)
         }
     })
 }
-{% assign tag_pages = site.documents | where: "layout", "tag"   %}
-const tag_pages = {{tag_pages | jsonify}};
 
-titles = {}
-tag_pages.forEach(function(page) {
-    titles[page.slug] = page.title;
+{% assign tagpages = site.documents | where: "layout", "tag"   %}
+const tagpages = {{tagpages | jsonify}};
+
+labels = {}
+tagpages.forEach(function(page) {
+    labels[page.slug] = page.title;
 });
 
 const tagnames = {{ site.data.tagnames | jsonify }};
@@ -52,4 +56,9 @@ const tagtree = {{ site.data.tagtree | jsonify }};
 const root = document.getElementById("tagtree");
 
 iterate(tagtree, root);
-console.log(total, "tags rendered");
+console.log(seen, "tags rendered");
+console.log(labels, "labels");
+
+let diff =  Object.keys(labels).filter(x => !seen.has(x));
+console.log(diff, "tags not rendered");
+
