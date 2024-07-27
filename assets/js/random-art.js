e3117ae0208d92e402bad3b02708b7ec6f763c97
selimslab <hselimozturk@gmail.com>
2024-07-27 19:46:48 +0200

    js

--- assets/js/random-art.js
+++ assets/js/random-art.js
@@ -1,16 +1,15 @@
 
-let cachedArt = null;
+let cachedJson = null;
 
 const shuffleArt = async () => {
-  if (!cachedArt) {
+  if (!cachedJson) {
     try {
       const response = await fetch("/assets/data/artworks.json", {
         headers: {
-          'Content-Type': 'application/json',
-          'cache': "force-cache"
+          'Content-Type': 'application/json'
         }
       });
-      cachedArt = await response.json();
+      cachedJson = await response.json();
     } catch (error) {
       console.error("Error fetching art:", error);
       return;
@@ -21,17 +20,27 @@ const shuffleArt = async () => {
   if (artIdx) {
     artIdx = parseInt(artIdx);
   } else {
-    artIdx = Math.floor(Math.random() * cachedArt.length);
+    artIdx = Math.floor(Math.random() * cachedJson.length);
   }
 
-  let nextIdx = (artIdx + 1) % cachedArt.length;
+  let nextIdx = (artIdx + 1) % cachedJson.length;
   localStorage.setItem('artIdx', nextIdx.toString());
 
   let img = document.getElementById("artwork");
-  artwork = cachedArt[artIdx];
+  artwork = cachedJson[artIdx];
   img.src = artwork.path;
   img.alt = artwork.name;
   document.getElementById("description").innerHTML = artwork.name;
+
+  // fetch the next image
+  try {
+    await fetch(cachedJson[nextIdx].path, {
+    });
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+
 };
 
 
