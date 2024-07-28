bab6c230d8e6bc8d515a9f0b7bb00ad219d06f9b
selimslab <hselimozturk@gmail.com>
2024-07-28 13:55:36 +0200

    js

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,19 +1,30 @@
 
 const dice = document.getElementById("dice");
+let cachedUrls = null;
 
-getRandomPage = async () =>{
-    animate(dice, "shakex");
-    fetch("/assets/data/urls.json", {headers:{     
-            'Content-Type': 'application/json',
-            'cache': "force-cache"
-    }})
-    .then(response => {
-    return response.json();
-    })
-    .then(urls => {
-        location.href = get_random_item(urls);
+getRandomPage = async () =>{    
+    if (!cachedUrls) {
+        try {
+            const response = await fetch("/assets/data/urls.json", {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            cachedUrls = await response.json();
+        } catch (error) {
+            console.error("Error fetching urls:", error);
+            return;
+        }
     }
-    );
+
+    animate(dice, "shakex");
+    const body = document.querySelector('body');
+    body.style.opacity = "20%";
+    // wait for 1 second
+    setTimeout(() => {
+        location.href = get_random_item(cachedUrls);
+    }, 200);
+
 }
 
 dice.addEventListener('keydown', function(e) {
