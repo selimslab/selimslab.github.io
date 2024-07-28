db5a0e96ce256d90b5cfa8c141ab6bfacb03cc1d
selimslab <hselimozturk@gmail.com>
2024-07-28 17:41:51 +0200

    type

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -8,7 +8,7 @@ async function getNextSentence() {
     content = content.replace(/\si\s/g, ' I ');
     // capitalize first letter of the sentence
     content = content.charAt(0).toUpperCase() + content.slice(1);
-    
+
     // turn new lines into commas 
     content = content.replace(/\n/g, ',');
     // remove if a space before a comma
@@ -26,14 +26,15 @@ document.addEventListener('DOMContentLoaded', async () => {
     let sentence = await getNextSentence();
     const sentenceDiv = document.getElementById('sentence');
     const wpmDiv = document.getElementById('wpm');
-    
+
     let typedText = '';
     let startTime = null;
     let endTime = null;
-    
+    let isHandlingKeydown = false;
+
     function renderSentence() {
         let formattedSentence = '';
-        
+
         for (let i = 0; i < sentence.length; i++) {
             if (i < typedText.length) {
                 if (typedText[i] === sentence[i]) {
@@ -45,10 +46,10 @@ document.addEventListener('DOMContentLoaded', async () => {
                 formattedSentence += sentence[i];
             }
         }
-        
+
         sentenceDiv.innerHTML = formattedSentence;
     }
-    
+
     async function reset() {
         sentence = await getNextSentence();
         typedText = '';
@@ -56,22 +57,30 @@ document.addEventListener('DOMContentLoaded', async () => {
         endTime = null;
         sentenceDiv.textContent = sentence;
     }
-    
+
+
     document.addEventListener('keydown', async (event) => {
+        if (isHandlingKeydown) return;
+        isHandlingKeydown = true;
+        console.log("event.key:", event.key);
+        
         if (!startTime) {
             startTime = new Date();
         }
 
-        if (event.key.length === 1) {
-            // only add the character if it is a letter or number or space or comma 
-            if (event.key.match(/^[a-zA-Z0-9, ]$/)) {
-                typedText += event.key;
-            }
-        } else if (event.key === 'Backspace') {
+        if (event.key === 'Backspace') {
             typedText = typedText.slice(0, -1);
+        } else if (event.key.length === 1 && event.key.match(/^[a-zA-Z0-9, ]$/)) {
+            // only add the character if it is a letter or number or space or comma 
+            typedText += event.key;
+        }  else {
+            isHandlingKeydown = false
+            return;
         }
-        
+
         renderSentence();
+        console.log("typedText:", typedText);
+        console.log("sentence:", sentence);
 
         if (typedText === sentence) {
             endTime = new Date();
@@ -85,6 +94,9 @@ document.addEventListener('DOMContentLoaded', async () => {
             console.error("typedText:", typedText);
             console.error("sentence:", sentence);
         }
+
+        isHandlingKeydown = false;
+
     });
 
     reset();
