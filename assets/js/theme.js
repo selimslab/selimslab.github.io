740a575eebd08b46cf0a6c627d9b1593d66a13fc
Selim <hselimozturk@gmail.com>
2025-08-23 21:41:10 +0200

    theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -11,24 +11,11 @@ function getSystemTheme() {
     return window.matchMedia('(prefers-color-scheme: dark)').matches ? THEME_DARK : THEME_LIGHT;
 }
 
-function setTheme() {
-    const theme = localStorage.getItem(THEME) || THEME_DARK;
-    
-    if (theme === THEME_LIGHT) {
-        setLightTheme()
-    } else {
-        setDarkTheme()
-    }
-}
 
-function setLightTheme(){
-    document.documentElement.setAttribute(DATA_THEME, THEME_LIGHT);
-    window.localStorage.setItem(THEME, THEME_LIGHT);
-}
-
-function setDarkTheme(){
-    document.documentElement.setAttribute(DATA_THEME, THEME_DARK);
-    window.localStorage.setItem(THEME, THEME_DARK);
+function setTheme(){
+    const theme = getSystemTheme()
+    document.documentElement.setAttribute(DATA_THEME, theme);
+    window.localStorage.setItem(THEME, theme);
 }
 
 
@@ -50,5 +37,4 @@ if (themeToggle) {
     });
 }
 
-setDarkTheme()
-
+setTheme()
