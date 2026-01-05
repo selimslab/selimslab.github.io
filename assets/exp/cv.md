3db4a9d7cc14f155fa8b1f94e6a0e75945f9feab
Selim <hselimozturk@gmail.com>
2026-01-05 14:11:52 +0100

    a

--- assets/exp/cv.md
+++
@@ -1,3 +0,0 @@
----
----
-[Download](https://docs.google.com/document/d/1tE9qhoUV_HaIxQkehaO_W7sJAVDuV6lpo22HGc36u3U/export?format=pdf)
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,32 +1,12 @@
-const DATA_THEME = 'data-theme';
-
 function getSystemTheme() {
     return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 }
 
 function setTheme(theme){
+    const DATA_THEME = 'data-theme';
     document.documentElement.setAttribute(DATA_THEME, theme);
     window.localStorage.setItem('theme', theme);
     console.log("setTheme: ", theme)
-}
-
-function toggleTheme() {
-    let currentMode = document.documentElement.getAttribute(DATA_THEME);
-    if (currentMode === 'dark') {
-        setTheme('light')
-    } else {
-        setTheme('dark')
-    }
-}
-
-const themeToggle = document.getElementById("themeToggle");
-if (themeToggle) {
-    themeToggle.textContent = "🌓";
-    themeToggle.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-            toggleTheme();
-        }
-    });
 }
 
 const theme = getSystemTheme()
